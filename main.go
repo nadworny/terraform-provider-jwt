01abc89d564536afcb5b6e40f6ab10253de66bad
Bartosz Nadworny <bartosz.nadworny@zurich.ch>
2020-08-24 16:41:05 +0200

    Fix plugin repo

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/nadworny/terraform-provider-jwt/jwt"
-	"github.com/hashicorp/terraform/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 )
 
 func main() {
